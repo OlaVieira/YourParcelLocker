948fe49e1f63eeabd9c8330b8b39df84c9a5d231
Aleksandra Zwierzchowska <a.zwierzchowska32@gmail.com>
2022-05-07 12:38:11 +0200

    Create Home Page Component, Button and the title. Add styles.

--- src/components/HomeComponent/Home.js
+++ src/components/HomeComponent/Home.js
@@ -1,12 +1,11 @@
 import './Home.css'
+import {Button} from "../Button/Button";
 
 export const Home = () => {
     return (
         <div className="Home">
             <h2 className="HelloMessage">Hello!</h2>
-            <button className="MainButton">
-                Pick up the package
-            </button>
+            <Button/>
             <i className="Arrow fa-solid fa-arrow-up"></i>
         </div>
 
