a53b70597473c6f7d39e996a3b8e72ca85d5f816
Aleksandra Zwierzchowska <a.zwierzchowska32@gmail.com>
2022-05-07 12:02:14 +0200

    Create Home Page Component and the title. Add styles.

---
+++ src/components/HomeComponent/Home.js
@@ -0,0 +1,14 @@
+import './Home.css'
+
+export const Home = () => {
+    return (
+        <div className="Home">
+            <h2 className="HelloMessage">Hello!</h2>
+            <button className="MainButton">
+                Pick up the package
+            </button>
+            <i className="Arrow fa-solid fa-arrow-up"></i>
+        </div>
+
+    );
+}
---
+++ src/components/PacelLockerH1Component/ParcelLockerH1.js
@@ -0,0 +1,9 @@
+import './ParcelLockerH1.css'
+
+export const ParcelLockerH1 = () => {
+    return (
+        <h1>
+            Your Parcel Locker
+        </h1>
+    );
+}
