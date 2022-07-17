fead3b77cfe4c4de0be62a820fc3c075a43e9b09
Aleksandra Zwierzchowska <a.zwierzchowska32@gmail.com>
2022-07-17 15:15:00 +0200

    Fix files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
 import {ParcelLockerH1} from "./components/PacelLockerH1/ParcelLockerH1";
 import {Home} from "./components/Home/Home";
 
@@ -6,7 +8,9 @@ export const App = () => {
   return (
     <div className="App">
       <ParcelLockerH1/>
-      <Home/>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        </Routes>
     </div>
   );
 }
